5e5617736487392356e4991ef8564a7d3614b2bb
liujohnson118 <j.liu@ivrnet.com>
2020-03-14 15:26:43 -0600

    get amount by currency

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express');
-
 const app = express();
+const request = require('request');
 
 app.get('/api/bitcoin', (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
-
-  res.json(customers);
+  request('http://api.bitcoincharts.com/v1/weighted_prices.json', { json: true }, (error, response, body) => {
+    if (error) { return console.log(error); }
+    res.send(body[req.query.currency]['30d']);
+  });
 });
 
 const port = 5000;
